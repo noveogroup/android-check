af8df3058ff97fd1bb93139243ddf5f565a65d7c
Pavel Stepanov <stefan.nsk@gmail.com>
2014-11-24 15:54:52 +0600

    init project

---
+++ android-check-plugin-example/app/src/main/java/com/noveogroup/example/MainActivity.java
@@ -0,0 +1,14 @@
+package com.noveogroup.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
