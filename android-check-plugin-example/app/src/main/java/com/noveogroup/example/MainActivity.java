b0cf17dc733115ba593c9ac8731eb572c7bae644
Pavel Stepanov <stefan.nsk@gmail.com>
2014-11-28 16:13:28 +0600

    add example for android-checkstyle

--- android-check-plugin-example/app/src/main/java/com/noveogroup/example/MainActivity.java
+++ android-check-plugin-example/app/src/main/java/com/noveogroup/example/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    // CR Code Review
+
+    // TODO To Do
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
