2e072eb974d359f710a0bffc00d5d21bf0284ffe
Pavel Stepanov <stefan.nsk@gmail.com>
2015-09-24 16:23:10 +0600

    fix resource search

--- android-check-plugin/src/main/groovy/com/noveogroup/android/check/common/Utils.groovy
+++ android-check-plugin/src/main/groovy/com/noveogroup/android/check/common/Utils.groovy
@@ -36,11 +36,17 @@ final class Utils {
     private Utils() { throw new UnsupportedOperationException() }
 
     static String getResource(Project project, String resourcePath) {
-        Set<File> files = project.rootProject.buildscript.configurations.classpath.resolve()
+        Set<File> files = new HashSet<>()
+        files += project.buildscript.configurations.classpath.resolve()
+        files += project.rootProject.buildscript.configurations.classpath.resolve()
         File file = files.find { new JarFile(it).getJarEntry(resourcePath) }
-        JarFile jarFile = new JarFile(file)
-        JarEntry jarEntry = jarFile.getJarEntry(resourcePath)
-        return jarFile.getInputStream(jarEntry).text
+        if (file == null) {
+            return null
+        } else {
+            JarFile jarFile = new JarFile(file)
+            JarEntry jarEntry = jarFile.getJarEntry(resourcePath)
+            return jarFile.getInputStream(jarEntry).text
+        }
     }
 
     static List<File> getAndroidSources(Project project) {
