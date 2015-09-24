c0bdefa2fbe8dafdd81f9d663a4d7a09cf044372
Pavel Stepanov <stefan.nsk@gmail.com>
2015-09-24 16:11:02 +0600

    implement report, reportXML, reportHTML into configuration

--- android-check-plugin/src/main/groovy/com/noveogroup/android/check/common/CommonConfig.groovy
+++ android-check-plugin/src/main/groovy/com/noveogroup/android/check/common/CommonConfig.groovy
@@ -78,6 +78,22 @@ class CommonConfig {
         this.configSeverity = severity
     }
 
+    File reportDirectory
+    File reportXML
+    File reportHTML
+
+    void report(File reportDirectory) {
+        this.reportDirectory = reportDirectory
+    }
+
+    void reportXML(File reportXML) {
+        this.reportXML = reportXML
+    }
+
+    void reportHTML(File reportHTML) {
+        this.reportHTML = reportHTML
+    }
+
     boolean resolveSkip(boolean defaultSkip) {
         return skip == null ? defaultSkip : skip
     }
@@ -130,12 +146,24 @@ class CommonConfig {
         return file
     }
 
+    private File resolveReportFile(String extension, File reportFile, File reportDirectory, String code) {
+        if (reportFile) {
+            return reportFile
+        }
+
+        if (reportDirectory) {
+            return new File(reportDirectory, "${code}.${extension}")
+        }
+
+        return new File(project.buildDir, "outputs/${code}/${code}.${extension}")
+    }
+
     File resolveXmlReportFile(String code) {
-        return new File(project.buildDir, "outputs/$code/${code}.xml")
+        return resolveReportFile('xml', reportXML, reportDirectory, code)
     }
 
     File resolveHtmlReportFile(String code) {
-        return new File(project.buildDir, "outputs/$code/${code}.html")
+        return resolveReportFile('html', reportHTML, reportDirectory, code)
     }
 
     List<File> getAndroidSources() {
